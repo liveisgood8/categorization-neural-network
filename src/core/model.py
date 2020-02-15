23b27746074628c76207bbb0702ce57030a60833
Kirill <lemeshonok12@gmail.com>
2020-02-15 15:44:02 +0700

    core: passing input shape in make_model function

--- src/core/model.py
+++ src/core/model.py
@@ -4,11 +4,11 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
 
-def make_cnn_model(img_width: int, img_height: int, num_classes: int) -> Sequential:
+def make_cnn_model(img_width: int, img_height: int, num_classes: int, input_shape: tuple) -> Sequential:
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
-                     input_shape=(1, img_width, img_height)))
+                     input_shape=input_shape))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
