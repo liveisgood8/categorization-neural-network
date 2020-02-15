07dbdc6f6d01698c4f228d6ddad5b54fdc6a7c7c
Kirill <lemeshonok12@gmail.com>
2020-02-15 16:40:09 +0700

    core: learn data to categorical, model fitting, model fixes

--- src/core/preparers/dict_set_to_learn_set.py
+++ src/core/preparers/dict_set_to_learn_set.py
@@ -1,12 +1,14 @@
 import numpy as np
 import random
 from keras import backend as keras_backend
+from keras.utils import to_categorical
 from typing import Dict, List
 
 import src.core.config as config
 
 
 def convert(data_set: Dict[int, List[np.ndarray]]) -> (np.ndarray, int):
+    num_classes = len(data_set)
     data_array = []
 
     for key in data_set.keys():
@@ -15,17 +17,18 @@ def convert(data_set: Dict[int, List[np.ndarray]]) -> (np.ndarray, int):
 
     random.shuffle(data_array)
 
-    data_array_labels = []
-    data_np_array = np.empty(shape=(len(data_set) * len(data_set[0]), config.IMG_WIDTH, config.IMG_HEIGHT),
+    data_np_array_labels = np.empty(shape=(num_classes * len(data_set[0]), num_classes))
+    data_np_array = np.empty(shape=(num_classes * len(data_set[0]), config.IMG_WIDTH, config.IMG_HEIGHT),
                              dtype=np.float32)
     for i, e in enumerate(data_array, start=0):
         class_label = list(e.keys())[0]
         data_np_array[i] = e[class_label]
-        data_array_labels.append(class_label)
+
+        data_np_array_labels[i] = to_categorical(class_label, num_classes)
 
     if keras_backend.image_data_format() == 'channels_first':
         data_np_array = np.reshape(data_np_array, (data_np_array.shape[0], 1, config.IMG_WIDTH, config.IMG_HEIGHT))
     else:
         data_np_array = np.reshape(data_np_array, (data_np_array.shape[0], config.IMG_WIDTH, config.IMG_HEIGHT, 1))
 
-    return data_np_array, data_array_labels
+    return data_np_array, data_np_array_labels
--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
+import src.core.config as config
+
+from keras import backend as keras_backend
 from src.core.parser.csv_dataset_parser import parse as csv_data_parse
 from src.core.preparers.split_dict import spit as split_dataset
 from src.core.preparers.dict_set_to_learn_set import convert as convert_to_learn_set
+from src.core.model import make_cnn_model
+from src.core.loaders import tf_loader
 
 num_classes = 65
+batch_size = 128
+epochs = 2
 data_set_file_path = '/home/nexus/HMCC balanced.csv'
 # data_set_file_path = '/media/Shared/Учеба/МКурс1_Семестр2/МашинноеОбучение/datasets/' \
 #                      'handwritten-mongolian-cyrillic-characters-database/HMCC balanced.csv'
+
+tf_loader.load()
 
 data_set = csv_data_parse(data_set_file_path, num_classes)
 
@@ -13,3 +22,21 @@ train_data_set, test_data_set = split_dataset(data_set, 4000)
 x_train, y_train = convert_to_learn_set(train_data_set)
 x_test, y_test = convert_to_learn_set(test_data_set)
 
+print('Train input shape:', x_train.shape)
+print('Train output shape:', y_train.shape)
+
+if keras_backend.image_data_format() == 'channels_first':
+    input_shape = (1, config.IMG_WIDTH, config.IMG_HEIGHT)
+else:
+    input_shape = (config.IMG_WIDTH, config.IMG_HEIGHT, 1)
+
+model = make_cnn_model(config.IMG_WIDTH, config.IMG_HEIGHT, num_classes, input_shape)
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
