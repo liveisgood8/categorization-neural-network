dd90fd21370aa8d3cdd0504eef589c5ab9755741
Kirill <lemeshonok12@gmail.com>
2020-02-15 12:37:25 +0700

    core: num classes passes as func arg

--- src/core/parser/csv_dataset_parser.py
+++ src/core/parser/csv_dataset_parser.py
@@ -2,9 +2,9 @@ import csv
 import numpy as np
 
 
-def parse(path: str):
+def parse(path: str, num_classes: int):
     with open(path, newline='\n') as csv_file:
-        data_set = prepare_dataset_dict()
+        data_set = prepare_dataset_dict(num_classes)
         data_reader = csv.reader(csv_file, delimiter=',')
         i = 0
         for row in data_reader:
@@ -22,9 +22,9 @@ def parse(path: str):
     return data_set
 
 
-def prepare_dataset_dict():
+def prepare_dataset_dict(num_classes: int):
     data_set_dict = {}
-    for i in range(0, 66):
+    for i in range(0, num_classes + 1):
         data_set_dict[i] = []
     return data_set_dict
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from src.core.parser.csv_dataset_parser import parse as csv_data_parse
 from src.core.splitters.split_dict import spit as split_dataset
 
+num_classes = 65
 
 data_set = csv_data_parse('/media/Shared/Учеба/МКурс1_Семестр2/МашинноеОбучение/datasets/'
-                                             'handwritten-mongolian-cyrillic-characters-database/HMCC balanced.csv')
+                                             'handwritten-mongolian-cyrillic-characters-database/HMCC balanced.csv',
+                          num_classes)
 
 train_data_set, test_data_set = split_dataset(data_set, 4000)
 
