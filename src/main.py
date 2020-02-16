9f351f1bcc78f92da8243b0c06f44fab861dc85e
Kirill <lemeshonok12@gmail.com>
2020-02-16 12:42:52 +0700

    core: main.py - prepare emnist letter data and train

--- src/main.py
+++ src/main.py
@@ -1,38 +1,47 @@
+import numpy as np
 import src.core.config as config
 
 from keras import backend as keras_backend
+from keras.utils import to_categorical
+from emnist import extract_training_samples
+from emnist import extract_test_samples
 from src.app.main_application import start as start_application
-from src.core.parser.csv_dataset_parser import parse as csv_data_parse
-from src.core.preparers.split_dict import spit as split_dataset
-from src.core.preparers.dict_set_to_learn_set import convert as convert_to_learn_set
-from src.core.model import make_cnn_model
+from src.core.model import make_cnn_model, load_model
 from src.core.model import save_model
 from src.core.loaders import tf_loader
 
 
+num_classes = 27
+batch_size = 128
+epochs = 8
+
+
 def train_and_save_mode():
-    num_classes = 66
-    batch_size = 128
-    epochs = 12
-    data_set_file_path = '/home/nexus/HMCC balanced.csv'
-    # data_set_file_path = '/media/Shared/Учеба/МКурс1_Семестр2/МашинноеОбучение/datasets/' \
-    #                      'handwritten-mongolian-cyrillic-characters-database/HMCC balanced.csv'
+    x_train, y_train = extract_training_samples('letters')
+    x_test, y_test = extract_test_samples('letters')
 
-    tf_loader.load()
+    x_train = np.copy(x_train).astype(np.float32)
+    x_test = np.copy(x_test).astype(np.float32)
 
-    data_set = csv_data_parse(data_set_file_path, num_classes)
+    x_train /= 255
+    x_test /= 255
 
-    train_data_set, test_data_set = split_dataset(data_set, 4000)
-    x_train, y_train = convert_to_learn_set(train_data_set)
-    x_test, y_test = convert_to_learn_set(test_data_set)
+    y_train = to_categorical(y_train, num_classes)
+    y_test = to_categorical(y_test, num_classes)
+
+    if keras_backend.image_data_format() == 'channels_first':
+        x_train = np.reshape(x_train, (x_train.shape[0], 1, config.IMG_WIDTH, config.IMG_HEIGHT))
+        x_test = np.reshape(x_test, (x_test.shape[0], 1, config.IMG_WIDTH, config.IMG_HEIGHT))
+        input_shape = (1, config.IMG_WIDTH, config.IMG_HEIGHT)
+    else:
+        x_train = np.reshape(x_train, (x_train.shape[0], config.IMG_WIDTH, config.IMG_HEIGHT, 1))
+        x_test = np.reshape(x_test, (x_test.shape[0], config.IMG_WIDTH, config.IMG_HEIGHT, 1))
+        input_shape = (config.IMG_WIDTH, config.IMG_HEIGHT, 1)
 
     print('Train input shape:', x_train.shape)
     print('Train output shape:', y_train.shape)
-
-    if keras_backend.image_data_format() == 'channels_first':
-        input_shape = (1, config.IMG_WIDTH, config.IMG_HEIGHT)
-    else:
-        input_shape = (config.IMG_WIDTH, config.IMG_HEIGHT, 1)
+    print('Test input shape:', x_test.shape)
+    print('Test output shape:', y_test.shape)
 
     model = make_cnn_model(config.IMG_WIDTH, config.IMG_HEIGHT, num_classes, input_shape)
     model.fit(x_train, y_train,
@@ -49,6 +58,8 @@ def train_and_save_mode():
 
 
 def main():
+    tf_loader.load()
+    train_and_save_mode()
     start_application()
 
 
